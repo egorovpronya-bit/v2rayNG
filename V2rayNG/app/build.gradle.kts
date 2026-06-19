9bf0b4e471719e12b1ebe628a1fad6e545de2b0d
egorovpronya-bit <egorovpronya@gmail.com>
2026-06-19 19:37:22 +0900

    Update build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.saqanet.vpn"
+        applicationId = "com.v2ray.ang"
         minSdk = 24
         targetSdk = 37
         versionCode = 734
