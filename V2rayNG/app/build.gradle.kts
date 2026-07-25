fa465e403f75955f45b913d02d215b6c8a19f103
egorovpronya-bit <egorovpronya@gmail.com>
2026-07-25 15:49:23 +0900

    bump version to 2.3.26

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.saqanet.vpn"
         minSdk = 24
         targetSdk = 37
-        versionCode = 785
-        versionName = "2.3.24"
+        versionCode = 786
+        versionName = "2.3.26"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
